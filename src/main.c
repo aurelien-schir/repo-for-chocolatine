6ff7a330a3e17f231bd4ace74ef17bbc43b4cadb
Aurelien <aurelien.schirmann@epitech.eu>
2026-04-02 15:06:12 +0200

    test for miror

--- src/main.c
+++ src/main.c
@@ -11,9 +11,9 @@ int main(int argc, char **argv)
 {
     if (argc > 1 && argv[1][0] == '-'
         && argv[1][1] == '4' && argv[1][2] == '2') {
-        write(1, "Vive la piscine !\n", my_strlen("Vive la piscine !\n"));
+        write(1, "vive la piscine !\n", my_strlen("vive la piscine !\n"));
         return 0;
     }
-    write(1, "Vive Epitech !\n", my_strlen("Vive Epitech !\n"));
+    write(1, "vive Epitech !\n", my_strlen("vive Epitech !\n"));
     return 0;
 }
